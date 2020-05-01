18b1607896e898d0e6bfbee06296b92983cc8940
Divyanshi Singh <divyanshipilot@gmail.com>
2020-05-01 16:08:11 +0530

    e-commerce app

---
+++ lib/components/horizontal_listview.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class HorizontalList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          Category(
+            image_location: 'images/cats/tshirt.png',
+            image_caption: 'shirt',
+          ),
+          Category(
+            image_location: 'images/cats/dress.png',
+            image_caption: 'dress',
+          ),
+          Category(
+            image_location: 'images/cats/jeans.png',
+            image_caption: 'pants',
+          ),
+          Category(
+            image_location: 'images/cats/formal.png',
+            image_caption: 'formal',
+          ),
+          Category(
+            image_location: 'images/cats/informal.png',
+            image_caption: 'informal',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Category extends StatelessWidget {
+  final String image_location;
+  final String image_caption;
+
+  Category({this.image_location, this.image_caption});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(2),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          width: 100,
+          child: ListTile(
+            title: Image.asset(
+              image_location,
+              width: 100,
+              height: 80,
+            ),
+            // subtitle: Text(image_caption),
+            subtitle: Container(
+              alignment: Alignment.topCenter,
+              child: Text(image_caption),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
